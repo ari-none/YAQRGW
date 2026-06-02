1c80de363536ab2eb54a7ed553087d15d20e0093
ruflun <enzohochin@gmail.com>
2026-06-02 11:39:48 +0200

    Added main.js and implemented a bit of scripting (still WIP)

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const qrapi = "https://quickchart.io/qr?"; // API url string
+
+console.log("Test");
+$("body").append("<h1>If you can see this, JQuery works.</h1>");
+
+// TODO: Do some basic code stuff
