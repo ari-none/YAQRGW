d48ead518f4abd61abb927a5c521b35cfa8cef6d
ruflun <enzohochin@gmail.com>
2026-06-03 11:38:56 +0200

    Added functional basic QR code generation

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,24 @@
-const qrapi = "https://quickchart.io/qr?"; // API url string
+const qrAPI = "https://quickchart.io/qr?"; // API url string
 const toastNotext = new bootstrap.Toast(document.getElementById("toastNoText")); // Toast object (no text set error)
 const toastGenerated = new bootstrap.Toast(document.getElementById("toastGenerated")); // Toast object (generated confirmation)
 
-console.log("Test");
-
-
-// TODO: Do some basic code stuff
-
 // When the QR code generate button is clicked
-async function qrcodeGenerateButtonClick() {
-    const qrText = $("#qrTextContent").val();
+async function qrGenerateButtonClick() {
+    var qrText = $("#qrTextContent").val();
 
     if (qrText.trim().length <= 0) {
         toastNotext.show();
     } else {
+        var qrURL = qrAPI;
+        const qrSize = $("input[name='qrSize']:checked").val();
+
+        qrURL += `text=${encodeURIComponent(qrText)}&`; // Text value
+        qrURL += `format=${$("input[name='qrFileFormat']:checked").val()}&` // File format
+        qrURL += `size=${qrSize}&`
+
+        $("#qrOutputImage").attr("src", qrURL); // TODO: Make it so the image in the output dynamically scales (up to 512px maximum)
+        
         toastGenerated.show();
     }
-
-    console.log(qrText.trim().length);
 }
-$("#qrcodeGenerateButton").on("click", qrcodeGenerateButtonClick)
+$("#qrGenerateButton").on("click", qrGenerateButtonClick)
