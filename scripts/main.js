f920c52feb509a080fc99fbe5ab5dcb3319337b2
ruflun <enzohochin@gmail.com>
2026-06-03 11:02:30 +0200

    Added all elements for the essential QR code settings and added a sample script for the verify button

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,22 @@
 const qrapi = "https://quickchart.io/qr?"; // API url string
+const toastNotext = new bootstrap.Toast(document.getElementById("toastNoText")); // Toast object (no text set error)
+const toastGenerated = new bootstrap.Toast(document.getElementById("toastGenerated")); // Toast object (generated confirmation)
 
 console.log("Test");
-$("body").append("<h1>If you can see this, JQuery works.</h1>");
+
 
 // TODO: Do some basic code stuff
+
+// When the QR code generate button is clicked
+async function qrcodeGenerateButtonClick() {
+    const qrText = $("#qrTextContent").val();
+
+    if (qrText.trim().length <= 0) {
+        toastNotext.show();
+    } else {
+        toastGenerated.show();
+    }
+
+    console.log(qrText.trim().length);
+}
+$("#qrcodeGenerateButton").on("click", qrcodeGenerateButtonClick)
