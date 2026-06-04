557ccb016367a14a0d0396e56cbd8835759e2cc7
ruflun <enzohochin@gmail.com>
2026-06-04 15:47:33 +0200

    Added color selection & started to work on some utility functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,48 @@
+// Global constants & stuff
 const qrAPI = "https://quickchart.io/qr?"; // API url string
 const toastNotext = new bootstrap.Toast(document.getElementById("toastNoText")); // Toast object (no text set error)
 const toastGenerated = new bootstrap.Toast(document.getElementById("toastGenerated")); // Toast object (generated confirmation)
+
+
+// Utility functions
+function checkValidImageURL(urlString) { // FIXME: Not returning boolean
+    $.ajax({
+        type: "HEAD",
+        url: urlString,
+        success: function(message,text,response){
+            return response.getResponseHeader('Content-Type').indexOf("image") != -1;
+        } 
+    });
+}
+
+function isStringEmpty(str) {
+    return str.trim().length <= 0
+}
+
 
 // When the QR code generate button is clicked
 async function qrGenerateButtonClick() {
     var qrText = $("#qrTextContent").val();
 
-    if (qrText.trim().length <= 0) {
+    if (isStringEmpty(qrText)) {
         toastNotext.show();
     } else {
         var qrURL = qrAPI;
-        const qrSize = $("input[name='qrSize']:checked").val();
 
-        qrURL += `text=${encodeURIComponent(qrText)}&`; // Text value
-        qrURL += `format=${$("input[name='qrFileFormat']:checked").val()}&` // File format
-        qrURL += `size=${qrSize}&`
+        // QR code essentials
+        qrURL += `text=${ encodeURIComponent(qrText) }&`; // Text value
+        qrURL += `format=${ $("input[name='qrFileFormat']:checked").val() }&` // File format
+        qrURL += `size=${ $("input[name='qrSize']:checked").val() }&`
 
-        $("#qrOutputImage").attr("src", qrURL); // TODO: Make it so the image in the output dynamically scales (up to 512px maximum)
+        // Colors
+        qrURL += `dark=${ $("#qrColorPrimary").val().slice(1) }&`;
+        qrURL += `light=${ $("#qrColorSecondary").val().slice(1) }&`;
+        if ($("#qrColorFinderUsesPrimary").is(":checked")) {
+            qrURL += `finderColor=${ $("#qrColorFinder").val().slice(1) }&`;
+        }
+
+        // Finalizing everything
+        $("#qrOutputImage").attr("src", qrURL);
         
         toastGenerated.show();
     }
